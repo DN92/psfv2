a365fbecd0a6ac9210b0b0d4417b92b4fb2d1559
dn92 <anatoly.tsinker13@gmail.com>
2023-05-29 05:22:28 -0400

    iframe on homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,24 @@
-export default function Home() {
+import styles from './home.module.css';
+
+const Home: React.FC = () => {
   return (
     <main>
-      <div>
-        PH
+      <div className={styles.iframeWrapper}>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/H2NcnwP9-Eo"
+          title="YouTube video player"
+          frameBorder="0" // copied from youtube source, will not edit unless there is an issue.
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          // this iFrame code was copied from youtube itself. Will ignore the TS error for now.
+          // its possible that react uses allowFullScreen instead of the html standard. TODO: recheck this later.
+          allowfullscreen
+          autoPlay
+        />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
